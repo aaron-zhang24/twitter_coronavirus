d593f286ac68ec950383cadb320d19d20feb61a1
aaron-zhang24 <aazhang@g.hmc.edu>
2026-02-25 19:28:22 -0800

    added first plot

---
+++ src/run_maps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for file in /data/Twitter\ dataset/geoTwitter20-*.zip; do
+    nohup python map.py --input_path "$file" --output_folder ../outputs &
+done
--- src/visualize.py
+++ src/visualize.py
@@ -11,6 +11,9 @@ args = parser.parse_args()
 # imports
 import os
 import json
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from collections import Counter,defaultdict
 
 # open the input path
@@ -26,3 +29,20 @@ if args.percent:
 items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
 for k,v in items:
     print(k,':',v)
+
+# generate top 10 item keys and values
+top_ten = sorted(items[:10], key =lambda item: item[1])
+labels, values = [item[0] for item in top_ten], [item[1] for item in top_ten]
+
+# plot the top 10 keys and values
+plt.figure(figsize=(12,6))
+plt.bar(labels, values)
+plt.title(args.key)
+plt.xlabel('Language' if 'lang' in args.input_path else 'Country')
+plt.ylabel('Count')
+plt.xticks(rotation=45, ha='right')
+
+# save output as png
+output_path = args.input_path + '.' + args.key.replace('#','') + '.png'
+plt.savefig(output_path)
+print('saved figure to:', output_path)
